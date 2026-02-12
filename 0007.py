e547b1c109c9c827c58b700cd72e4e683f32b10d
Kaadz <kiwi092020@gmail.com>
2026-02-12 17:19:41 +0530

    Create 0007.py

---
+++ 0007.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+
+        reversed_num = int(str(abs(x))[::-1])
+        result = sign * reversed_num
+        
+        if result < -2**31 or result > 2**31 - 1:
+            return 0 
+
+        return result
+
+        
+        
